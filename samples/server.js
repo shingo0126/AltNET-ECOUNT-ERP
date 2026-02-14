2a363f6e1b0490921ac3efb6bcf16631d63aa28f
genspark_dev <genspark_dev@genspark.ai>
2026-02-14 03:03:02 +0000

    Pre-Glassmorphism 2.0 backup

---
+++ samples/server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+const SAMPLES_DIR = path.join(__dirname);
+
+const MIME_TYPES = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/login.html' : req.url;
+  filePath = path.join(SAMPLES_DIR, filePath.split('?')[0]);
+  
+  const ext = path.extname(filePath);
+  const contentType = MIME_TYPES[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end('<h1>404 Not Found</h1>');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Sample server running on port ${PORT}`);
+});
